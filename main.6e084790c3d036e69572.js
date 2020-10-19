5e1466dc4a67157fff22b087377cb02427ea355b
Simonenko Michail <Qwertyon1993@gmail.com>
2020-10-19 22:01:02 +0300

    Updates

--- main.6e084790c3d036e69572.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(n,e,t){},QfWi:function(n,e,t){"use strict";t.r(e);t("L1EO"),t("JBxO"),t("FdtR");var o=function(n){return new Promise((function(e,t){setTimeout((function(){e(n)}),n)}))},i=function(n){return console.log("Resolved after "+n+"ms")};o(2e3).then(i),o(1e3).then(i),o(1500).then(i)}},[["QfWi",1,2]]]);
-//# sourceMappingURL=main.6e084790c3d036e69572.js.map
